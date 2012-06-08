df5d9fb00fec64fb40ab345705a3d1b360161b0f
Tobias Brog <to.lokh@gmail.com>
2012-06-08 16:20:02 +0200

    Worked on DBTool

---
+++ SYO/src/syo_model/DBTool.java
@@ -0,0 +1,35 @@
+package syo_model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
+/**
+ * 
+ * @author kuepfers
+ */
+public class DBTool {
+
+	private Connection connection;
+	private Statement statement = null;
+	private ResultSet rSet = null;
+
+	public void connectDB(String host, String db, String user, String password) throws InstantiationException, IllegalAccessException {
+		 try {
+	            Class.forName("com.mysql.jdbc.Driver").newInstance();
+	        } catch (ClassNotFoundException ex) {
+	            System.out.println("SQLException: " + ex.getMessage());
+	        }
+
+        try {
+        	connection = DriverManager.getConnection("jdbc:mysql://" + host + "/" + db + "?"
+                    + "user=" + user + "&password=" + password);
+
+        } catch (SQLException ex) {
+
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+	}
+}

3137d93f68ca6936accfc5c2f80978e4b76b45dd
Tobias Brog <to.lokh@gmail.com>
2012-06-13 08:39:26 +0200

    Arbeit an DBTool

--- SYO/src/syo_model/DBTool.java
+++ SYO/src/syo_model/DBTool.java
@@ -14,22 +14,54 @@ public class DBTool {
 	private Statement statement = null;
 	private ResultSet rSet = null;
 
-	public void connectDB(String host, String db, String user, String password) throws InstantiationException, IllegalAccessException {
-		 try {
-	            Class.forName("com.mysql.jdbc.Driver").newInstance();
-	        } catch (ClassNotFoundException ex) {
-	            System.out.println("SQLException: " + ex.getMessage());
-	        }
+	/**
+	 * 
+	 * @param host
+	 * @param db
+	 * @param user
+	 * @param password
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public void connectDB(String host, String db, String user, String password)
+			throws InstantiationException, IllegalAccessException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (ClassNotFoundException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
 
-        try {
-        	connection = DriverManager.getConnection("jdbc:mysql://" + host + "/" + db + "?"
-                    + "user=" + user + "&password=" + password);
+		try {
+			connection = DriverManager
+					.getConnection("jdbc:mysql://" + host + "/" + db + "?"
+							+ "user=" + user + "&password=" + password);
 
-        } catch (SQLException ex) {
+		} catch (SQLException ex) {
 
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+
+	/**
+	 * Closes the connection. Checks if there is an existing connection before
+	 * attempting to close it.
+	 */
+	public void closeDBconnection() {
+		if (rSet != null) {
+			try {
+				rSet.close();
+			} catch (SQLException sqlEx) {
+			}
+			rSet = null;
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException sqlEx) {
+			}
+			statement = null;
+		}
 	}
 }

3dbb25dfca7f5508c3a4551126e00548d1675168
Tobias Brog <to.lokh@gmail.com>
2012-06-13 10:39:17 +0200

    DBTool

--- SYO/src/syo_model/DBTool.java
+++ SYO/src/syo_model/DBTool.java
@@ -14,11 +14,28 @@ public class DBTool {
 	private Connection connection;
 	private Statement statement = null;
 	private ResultSet rSet = null;
+	private String host;
+	private String db;
+	private String user;
+	private String password;
 	private static DBTool instance;
 
+	/**
+	 * Constructor sets up the necessary data for the connection.
+	 */
 	private DBTool() {
+		host = "localhost";
+		db = "";
+		user = "root";
+		password = "1234";
 	}
 
+	/**
+	 * Returns the only instance of DBTool. Creates a DBTool-instance only if
+	 * there isn't one already.
+	 * 
+	 * @return DBTool The only instance of DBTool.
+	 */
 	public static DBTool getInstance() {
 		if (instance == null) {
 			instance = new DBTool();
@@ -36,11 +53,16 @@ public class DBTool {
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
 	 */
-	public void connectDB(String host, String db, String user, String password)
-			throws InstantiationException, IllegalAccessException {
+	private void connectDB() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (ClassNotFoundException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+			System.out.println("SQLException: " + ex.getMessage());
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
 			System.out.println("SQLException: " + ex.getMessage());
 		}
 
@@ -60,7 +82,7 @@ public class DBTool {
 	 * Closes the connection. Checks if there is an existing connection before
 	 * attempting to close it.
 	 */
-	public void closeDBconnection() {
+	private void closeDB() {
 		if (rSet != null) {
 			try {
 				rSet.close();
@@ -74,22 +96,33 @@ public class DBTool {
 			} catch (SQLException sqlEx) {
 			}
 			statement = null;
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	/**
 	 * Adds a Sammlung to the database.
-	 * @param name The name of the Sammlung.
+	 * 
+	 * @param name
+	 *            The name of the Sammlung.
 	 */
 	public void addSammlung(String name) {
-		
+		connectDB();
+
+		closeDB();
 	}
 
 	/**
 	 * Adds an object to the database.
 	 * 
-	 * @param name The name of the object.
-	 * @param typID The ID of the type of the object
+	 * @param name
+	 *            The name of the object.
+	 * @param typID
+	 *            The ID of the type of the object
 	 */
 	public void addObject(String name, String typID) {
 
@@ -98,28 +131,36 @@ public class DBTool {
 	/**
 	 * Adds a type to the database.
 	 * 
-	 * @param name The name of the type.
-	 * @param feldIDs The list of the fields that belong to this type.
+	 * @param name
+	 *            The name of the type.
+	 * @param feldIDs
+	 *            The list of the fields that belong to this type.
 	 */
 	public void addType(String name, ArrayList<String> feldIDs) {
 
 	}
-	
+
 	/**
 	 * Adds a field of the type Integer to the database.
-	 * @param name The name of the field.
-	 * @param length int the length of the field.
+	 * 
+	 * @param name
+	 *            The name of the field.
+	 * @param length
+	 *            int the length of the field.
 	 */
 	public void addIntegerField(String name, int length) {
-		
+
 	}
-	
+
 	/**
 	 * Adds a field of the type varchar to the database.
-	 * @param name The name of the field.
-	 * @param length int the length of the field.
+	 * 
+	 * @param name
+	 *            The name of the field.
+	 * @param length
+	 *            int the length of the field.
 	 */
 	public void addStringField(String name, int length) {
-		
+
 	}
 }

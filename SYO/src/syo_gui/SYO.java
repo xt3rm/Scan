96d72c83ad831712b84f1d91c77bf023a832e399
Michael Beck <michael.beck@bluewin.ch>
2012-06-14 16:05:57 +0200

    Alles Neu

--- SYO/src/syo_gui/SYO.java
+++ SYO/src/syo_gui/SYO.java
@@ -1,15 +1,10 @@
 package syo_gui;
 
-import syo_model.DBTool;
 
 public class SYO  {
 
 	public static void main(String[] args) {
-	/*	DBTool.getInstance().connectDB();
-		DBTool.getInstance().addObject("TestNeu", 1, 1);
-		DBTool.getInstance().closeDB();*/
-		//
-		new MainView("SYO");
-	
+		MainView a = new MainView();
+		a.setVisible(true);
 	}
 }

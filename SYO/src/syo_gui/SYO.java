60fec122654886a8354cd2f667ee244f050d125b
Tobias Brog <to.lokh@gmail.com>
2012-06-14 14:10:05 +0200

    Cleaned main

--- SYO/src/syo_gui/SYO.java
+++ SYO/src/syo_gui/SYO.java
@@ -1,15 +1,8 @@
 package syo_gui;
 
-import syo_model.DBTool;
-
 public class SYO  {
 
 	public static void main(String[] args) {
-	/*	DBTool.getInstance().connectDB();
-		DBTool.getInstance().addObject("TestNeu", 1, 1);
-		DBTool.getInstance().closeDB();*/
-		//
 		new MainView("SYO");
-	
 	}
 }

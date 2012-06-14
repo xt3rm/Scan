79fbd01f426540b1d13a79e1358c1038f3cc703c
Michael Beck <michael.beck@bluewin.ch>
2012-06-14 09:14:11 +0200

    Navigationslabel in GUI

--- SYO/src/syo_gui/MainView.java
+++ SYO/src/syo_gui/MainView.java
@@ -2,8 +2,12 @@ package syo_gui;
 
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -11,17 +15,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class MainView extends JFrame {
+public class MainView extends JFrame implements MouseListener{
 	
     //Tobi-Stuff---
 	private static final long serialVersionUID = 8892701173173371771L;
 
-	
-	//Panel---
 	JPanel pnlContent = new JPanel();
 	HeadlinePanel pnlHeadline;
     JPanel pnlNavigation = new JPanel();
     JPanel pnlView = new JPanel();
+    Label lblNavigation; 
 	
     
 	public MainView(String title) {
@@ -31,46 +34,64 @@ public class MainView extends JFrame {
 		this.setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
         		
+		
+/*
+*-------------------------------------------------------
+*  PANEL - CONTENT - pnlContent
+*-------------------------------------------------------       
+*/
+        
 		pnlContent.setLayout(null);
-        pnlContent.setSize(900,600);    
-        pnlNavigation.setLayout(null);
-        pnlView.setLayout(null);
-
-        pnlNavigation.setBounds(0,100,225,500);
-        pnlView.setBounds(230,100,700,500);
-
-        this.getContentPane().add(pnlNavigation);
-        pnlNavigation.setBackground(Color.WHITE);
-        
-        this.getContentPane().add(pnlView);
-        pnlView.setBackground(Color.WHITE);
-        
-        this.getContentPane().add(pnlContent);
+        pnlContent.setSize(900,600);  
         pnlContent.add(new MainView.HeadlinePanel());
-        pnlContent.setBackground(Color.lightGray);
+        pnlContent.setBackground(new Color(222,222,222));
+        pnlContent.add(pnlNavigation);
+        pnlContent.add(pnlView);
+        this.getContentPane().add(pnlContent);
         
-//-------------------------------------------------------
-//  PANEL - NAVIGATION - pnlNavigation
-//-------------------------------------------------------
+/*        
+*-------------------------------------------------------
+*  PANEL - NAVIGATION - pnlNavigation
+*-------------------------------------------------------
+*/
+        pnlNavigation.setLayout(null);
+        pnlNavigation.setBounds(0,100,225,500);
         
-    	
-    	
-//-------------------------------------------------------
-//  PANEL - CONTENT - pnlContent
-//-------------------------------------------------------
-   	//Ja, ich weiss, hier steht noch  nichts!!	
-    	
-    	
+        pnlNavigation.setBackground(Color.WHITE);
+        lblNavigation = new Label();
+        pnlNavigation.add(lblNavigation);
         
+        lblNavigation.setBackground(new Color(222,222,222));
+        lblNavigation.setForeground(new Color(0,0,0));
+        lblNavigation.setAlignment(1);
+        lblNavigation.setBounds(37, 10, 150, 30);
+        lblNavigation.setFont(new Font((lblNavigation.getFont()).getFontName(), (lblNavigation.getFont()).getStyle(), 16));
+    	lblNavigation.setText("Navigationspunkte");
         
+/*    	
+*-------------------------------------------------------
+*  PANEL - View - pnlView
+*-------------------------------------------------------	
+*/
+    	pnlView.setLayout(null);
+        pnlView.setBounds(230,100,700,500);
+        pnlView.setBackground(Color.WHITE);
+
+        repaint();
         
 	}
 	
-//-------------------------------------------------------
-//  PANEL - HEADLINE - HeadlinePanel	
-//-------------------------------------------------------
+	
+/*
+*-------------------------------------------------------
+*  PANEL - HEADLINE - HeadlinePanel	
+*-------------------------------------------------------
+*/	
+	
 	
 	class HeadlinePanel extends JPanel {
+		
+		private static final long serialVersionUID = -1802094992738102637L;
 		private Image theImage;
         
         public HeadlinePanel(){
@@ -92,9 +113,47 @@ public class MainView extends JFrame {
             g.drawImage(theImage, 0, 0, null); // see javadoc for more info on the parameters
         }
     }
+//----Ende - HEADLINE ---------------------------------------	
+
 	
-
+	
+	
 	public void paint(Graphics g) {
-
+		lblNavigation.setVisible(true);
     }
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
